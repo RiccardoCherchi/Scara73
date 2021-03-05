9256554014ceed6af06d66a9f1df4d9b50830163
RiccardoCherchi <51675691+RiccardoCherchi@users.noreply.github.com>
2021-03-05 09:04:36 +0100

    Update Highlights.vue

--- src/views/Highlights.vue
+++ src/views/Highlights.vue
@@ -95,11 +95,11 @@ export default {
     },
 
     handleScroll(event) {
-      if (event.deltaY < 0) {
+      if (event.deltaY > 0) {
         const index =  Math.min(this.currentIndex + 1, this.sections.length - 1);
         this.currentIndex = index;
       } else {
-        if (this.currentIndex > 0) {
+        if (this.currentIndex < 0) {
           this.currentIndex --;
         }
       }
